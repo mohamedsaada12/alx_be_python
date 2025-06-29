0502e614c3a0195c1addd52cc534f85979e16b2a
mohamedsaada12 <profsaada@gmail.com>
2025-06-29 06:28:59 +0300

    Feat: Add square pattern drawing using nested loops

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,16 @@
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()
+    row += 1
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()
+    row += 1
